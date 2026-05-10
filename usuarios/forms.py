07c1b78f3370ebd5f8108774e46131c6e717bfd7
Isaíasroot <Isaasroot@root.com>
2026-05-10 16:53:52 -0500

    Corregir configuración de base de datos y formulario de registro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,3 +5,6 @@ class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
+        }
